2266bf1d4fc713da49ce59ca0a123f243bfcda5e
themother <jacktowleretc@hotmail.com>
2019-09-01 10:02:54 +0100

    sessions

--- flask_play/main2.py
+++ flask_play/main2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
 from flask_script import Manager, Command, Shell
 from forms import ContactForm
 
@@ -76,7 +76,7 @@ def contact():
 def cookie():
     if not request.cookies.get('foo'):
         res = make_response('Setting a cookie')
-        res.set_cookie('foo', 'bar', max_age=60*60*24*365*2)
+        res.set_cookie('foo', 'bar', max_age=60 * 60 * 24 * 365 * 2)
     else:
         res = make_response(f"Value of cookie foo is {request.cookies.get('foo')}")
     return res
@@ -99,6 +99,35 @@ def article():
         return res, 302
 
     return render_template('article.html')
+
+
+@app.route('/visits-counter/')
+def visits():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1  # reading and updating session data
+    else:
+        session['visits'] = 1  # setting session data
+    return "Total visits: {}".format(session.get('visits'))
+
+
+@app.route('/session/')
+def updating_session():
+    res = str(session.items())
+
+    cart_item = {'pineapples': '10', 'apples': '20', 'mangoes': '30'}
+    if 'cart_item' in session:
+        session['cart_item']['pineapples'] = '100'
+        session.modified = True
+    else:
+        session['cart_item'] = cart_item
+
+    return res
+
+
+@app.route('/delete-visits/')
+def delete_visits():
+    session.pop('visits', None)  # delete visits
+    return 'Visits deleted'
 
 
 def shell_context():
